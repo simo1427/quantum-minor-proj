e9acdd97f28bfce4d6663b9f815e8f4f4e358e26
Codrin Ogreanu <C.Ogreanu@student.tudelft.nl>
2024-01-17 23:58:46 +0100

    Initial tests for animating operations on circuits

--- circuit_conversion.py
+++ circuit_conversion.py
@@ -2,42 +2,49 @@ from __future__ import annotations
 from typing import Any, Callable, Mapping, Generator, Tuple, Sequence, MutableSequence
 import numpy as np
 from numpy.typing import NDArray
+from qiskit import execute, Aer
 from qiskit.circuit import QuantumCircuit
+from qiskit.result import Result
 
 Image = Tuple[NDArray[np.uint8], NDArray[np.uint8], NDArray[np.uint8]]
 PixelMapping = Callable[[int], Sequence[str]]
 
-class CircuitBuilder:
 
+class CircuitBuilder:
+    '''
+    Provides a nice interface for building quantum circuits tailored specifically for this project.
+    '''
     def __init__(self, w_qubits: int, h_qubits: int) -> None:
         self.__w_qubits: int = w_qubits
         self.__h_qubits: int = h_qubits
         self.__data: MutableSequence[complex] = [0] * (2 ** self.qubits())
         self.__circuit: QuantumCircuit = QuantumCircuit(self.qubits())
-    
+
     def qubits(self) -> int:
         return self.__w_qubits + self.__h_qubits
-    
+
     def from_circuit(self, circuit: QuantumCircuit) -> CircuitBuilder:
         assert circuit.num_qubits == self.qubits()
         self.__circuit.data = [instr for instr in circuit.data if instr.operation.name != "initialize"]
         return self
-    
+
     def gates(self, function: Callable[[QuantumCircuit], Any]) -> CircuitBuilder:
         function(self.__circuit)
         return self
-    
+
     def with_data(self, data: Mapping[str, complex]) -> CircuitBuilder:
         for key, value in data.items():
             self.__data[int(key, 2)] = value
         return self
-    
+
     def build(self) -> QuantumCircuit:
         self.__circuit.initialize(self.__data, range(self.qubits()))
         self.__circuit.data.insert(0, self.__circuit.data.pop())
         return self.__circuit
 
-def _map_channel_to_amplitudes(channel: NDArray[np.uint8], w_strings: Sequence[str], h_strings: Sequence[str]) -> Mapping[str, complex]:
+
+def _map_channel_to_amplitudes(channel: NDArray[np.uint8], w_strings: Sequence[str], h_strings: Sequence[str]) -> \
+Mapping[str, complex]:
     data = {
         w_strings[x] + h_strings[y]: np.sqrt(channel[x, y], dtype=np.float64)
         for x in range(channel.shape[0])
@@ -48,10 +55,12 @@ def _map_channel_to_amplitudes(channel: NDArray[np.uint8], w_strings: Sequence[s
 
     for key, value in data.items():
         data[key] = value / norm
-    
+
     return data
 
-def _map_amplitudes_to_channel(state_vector: Sequence[complex], w_strings: Sequence[str], h_strings: Sequence[str]) -> NDArray[np.uint8]:
+
+def _map_amplitudes_to_channel(state_vector: Sequence[complex], w_strings: Sequence[str], h_strings: Sequence[str]) -> \
+NDArray[np.uint8]:
     m, n = len(w_strings), len(h_strings)
     rescale = 255 / np.max(np.power(np.abs(state_vector), 2))
 
@@ -63,6 +72,7 @@ def _map_amplitudes_to_channel(state_vector: Sequence[complex], w_strings: Seque
 
     return data
 
+
 def hamming_manhattan_mapping(n_qubits: int) -> Sequence[str]:
     bit_strings = ["0", "1"]
 
@@ -73,16 +83,40 @@ def hamming_manhattan_mapping(n_qubits: int) -> Sequence[str]:
 
     return bit_strings
 
-def to_circuit(channel: NDArray[np.uint8], pixel_mapping: PixelMapping = hamming_manhattan_mapping) -> CircuitBuilder:
-    w_qubits, h_qubits = int(np.ceil(np.log2(channel.shape[0]))), int(np.ceil(np.log2(channel.shape[1])))
-    data = _map_channel_to_amplitudes(channel, pixel_mapping(w_qubits), pixel_mapping(h_qubits))       
-    return CircuitBuilder(w_qubits, h_qubits).with_data(data)
-                                          
-def to_circuits(image: Image, pixel_mapping: PixelMapping = hamming_manhattan_mapping) -> Generator[CircuitBuilder, None, None]:
-    for channel in image:
-        yield to_circuit(channel, pixel_mapping)
 
-def to_channel(state_vector: Sequence[complex], width: int, height: int, pixel_mapping: PixelMapping = hamming_manhattan_mapping) -> NDArray[np.uint8]:
+def channel_to_circuit(channel: NDArray[np.uint8], pixel_mapping: PixelMapping = hamming_manhattan_mapping) -> CircuitBuilder:
+    '''
+    Converts a single channel into a `CircuitBuilder`. It can then be used to construct more complex circuits.
+    '''
+    w_qubits, h_qubits = int(np.ceil(np.log2(channel.shape[0]))), int(np.ceil(np.log2(channel.shape[1])))
+    data = _map_channel_to_amplitudes(channel, pixel_mapping(w_qubits), pixel_mapping(h_qubits))
+
+    return CircuitBuilder(w_qubits, h_qubits).with_data(data)
+
+
+def image_to_circuits(image: Image, pixel_mapping: PixelMapping = hamming_manhattan_mapping) -> Generator[CircuitBuilder, None, None]:
+    '''
+    Takes an entire `Image` type - a tuple of 3 channels - and converts them into 3 circuits.
+    '''
+    for channel in image:
+        yield channel_to_circuit(channel, pixel_mapping)
+
+
+def sv_to_channel(state_vector: Sequence[complex], width: int, height: int,
+                  pixel_mapping: PixelMapping = hamming_manhattan_mapping) -> NDArray[np.uint8]:
+    '''
+    Converts a state vector back into a channel.
+    '''
     w_qubits, h_qubits = int(np.ceil(np.log2(width))), int(np.ceil(np.log2(height)))
     assert len(state_vector) == 2 ** (w_qubits + h_qubits)
     return _map_amplitudes_to_channel(state_vector, pixel_mapping(w_qubits), pixel_mapping(h_qubits))[:width, :height]
+
+
+def run_circuit(qc: QuantumCircuit) -> Sequence[complex]:
+    '''
+    Runs the circuit using the `statevector_simulator` backend of Aer.
+    Important: unlike in most other cases, there should be no measurements done at the end of the QC
+    :return: the statevector of the circuit
+    '''
+    result: Result = execute(qc, Aer.get_backend('statevector_simulator')).result()
+    return result.get_statevector()
--- main.py
+++ main.py
@@ -1,22 +1,57 @@
+import numpy as np
+from apng import APNG
 from qiskit import QuantumCircuit
-from qiskit.quantum_info import Pauli
-from qiskit_ibm_runtime import QiskitRuntimeService, Sampler
 
-from qmputils import retrieve_token
-
-qc = QuantumCircuit(2)
-
-# Provide your IBM Quantum API token here.
-# These two lines only need to be run once, as your credentials get saved in $HOME/.qiskit/qiskit-ibm.json.
-# See more: https://docs.quantum.ibm.com/start/setup-channel
-api_token = retrieve_token()
-service = QiskitRuntimeService(channel="ibm_quantum", token=api_token)
-
-backend = service.backend("ibmq_qasm_simulator")
+from circuit_conversion import channel_to_circuit, run_circuit, sv_to_channel
+from image_preprocessing import image_read
+from PIL import Image as PilImage
 
 
-job = Sampler(backend).run(qc, shots=1024)
-print(f"job id: {job.job_id()}")
-result = job.result()
-print(result)
+def rotate_by_angle(phi):
+    '''
+    Having this wrapper that returns the actual gate sequence allows us to change the phi angle dynamically.
+    Change the inner method as needed.
+    :param phi: the angle in radians
+    '''
+    def inner_method(qc: QuantumCircuit):
+        qc.rx(phi, qc.qubits)
+    return inner_method
+
+
+def animate_image(filename: str):
+    circuit_builder = channel_to_circuit(image_read(filename, True))
+
+    # Animate the transformation. Still a WIP
+    files = []
+    for i in range(1, 101):
+        circuit = circuit_builder.gates(rotate_by_angle(2 * np.pi * i / 100)).build()
+
+        channels = sv_to_channel(run_circuit(circuit), 16, 16)
+
+        files.append(f'{i}.png')
+        PilImage.fromarray(channels).convert('L').save(f'media/{i}.png')
+    APNG.from_files(files, delay=50).save('media/result.png')
+
+if __name__ == '__main__':
+    animate_image('media/Flower.png')
+
+
+    # ================   Not needed for now   ================
+    #
+    #
+    # qc = QuantumCircuit(2)
+    #
+    # # Provide your IBM Quantum API token here.
+    # # These two lines only need to be run once, as your credentials get saved in $HOME/.qiskit/qiskit-ibm.json.
+    # # See more: https://docs.quantum.ibm.com/start/setup-channel
+    # api_token = retrieve_token()
+    # service = QiskitRuntimeService(channel="ibm_quantum", token=api_token)
+    #
+    # backend = service.backend("ibmq_qasm_simulator")
+    #
+    #
+    # job = Sampler(backend).run(qc, shots=1024)
+    # print(f"job id: {job.job_id()}")
+    # result = job.result()
+    # print(result)
 
