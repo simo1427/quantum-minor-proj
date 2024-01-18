3628fe57d2674307a28b5d65f382d30e4f1ed628
Simeon Atanasov <simeon.a.atanasov@gmail.com>
2024-01-18 15:33:24 +0100

    Added arguments to the effects
    Also removed functions that had hardcoded args, those can be replicated using the given methods, passed through a lambda

--- image_effects.py
+++ image_effects.py
@@ -1,41 +1,35 @@
 import numpy as np
 from qiskit import QuantumCircuit
+from typing import Callable
 
 def rx_gates(phi, qc: QuantumCircuit): # intermediate between the image and the same flipped
     qc.rx(phi, range(qc.num_qubits))
 
     # IMPORTANT: NO IMAGE WAS OUTPUT WHEN USING PI/16!! TODO: LOOK INTO WHY
 
-def ry_gates(qc: QuantumCircuit): # intermediate between the image and the same flipped
-    qc.ry(np.pi / 8, range(qc.num_qubits))
+def ry_gates(phi: float, qc: QuantumCircuit): # intermediate between the image and the same flipped
+    qc.ry(phi, range(qc.num_qubits))
 
     # IMPORTANT: A BLACK IMAGE WAS OUTPUT WHEN USING PI/8!! TODO: LOOK INTO WHY
 
-def single_rx_gate(qc: QuantumCircuit): # intermediate between the image and the same flipped
-    qc.rx(np.pi/2, 4)
+def single_rx_gate(phi: float, i: int, qc: QuantumCircuit): # intermediate between the image and the same flipped
+    qc.rx(phi, i)
 
     # IMPORTANT: OUTPUT DIDN'T SEEM TO CHANGE FOR PI/16! TODO: LOOK INTO WHY
     # possible explanation: the effect is barely visible
 
-def rz_gates(qc: QuantumCircuit): 
-    qc.rz(np.pi / 4, range(qc.num_qubits))
+def rz_gates(phi: float, qc: QuantumCircuit): 
+    qc.rz(phi, range(qc.num_qubits))
 
-def single_hadamard(qc: QuantumCircuit): # vertical bars of size 2^{qubit with hadamard gate}
-    qc.h(4)
-    # qc.rx(-np.pi/3, 0)
+def single_hadamard(i: int, qc: QuantumCircuit): # vertical bars of size 2^{qubit with hadamard gate}
+    qc.h(i)
 
 def some_mix_1(qc: QuantumCircuit): 
     qc.h(0)
     qc.rx(-np.pi/3, 4)
 
-def hadamard_last(qc: QuantumCircuit):
-    qc.h(qc.num_qubits-1)
-
-def rx_last_qubit(qc: QuantumCircuit):
-    qc.rx(np.pi, qc.num_qubits-1)
-
-def swap_circuit_first_last(qc: QuantumCircuit):
-    qc.swap(0, qc.num_qubits-2)
+def swap_gate(i: int, j: int, qc: QuantumCircuit):
+    qc.swap(i, j)
 
 def rotate_by_angle(phi):
     '''
@@ -44,4 +38,4 @@ def rotate_by_angle(phi):
     :param phi: the angle in radians
     '''
     
-    return lambda x: rx_gates(phi, x)
+    return lambda circuit, registers: rx_gates(phi, circuit)
